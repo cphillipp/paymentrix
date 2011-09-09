f0737d1e8321ac5d5d5fe1448a4e9171aedc62a1
unknown <phillipp@.(none)>
2011-09-09 15:44:14 -0600

    updated more files

---
+++ form.php
@@ -0,0 +1,20 @@
+<?php
+    session_start();
+    $to = "clayphillipp@gmail.com";
+    $subject = "Contact Us";
+    $email = $_POST['email'];
+    $message = $_POST['message'];
+    $name = $_POST['name'];
+    $phone = $_POST['phone'];
+    $message = $name . "\n ". $email . "\n ". $phone . "\n ". $message;
+    $headers = "From: $email";
+    $sent = mail($to, $subject, $message, $headers);
+    if($sent) {
+        $_SESSION['message'] = 'Thank You';
+        header('Location: contact.php#bottom');
+    }
+    else{
+        $_SESSION['message'] = 'Sorry';
+        header('Location: contact.php#bottom');
+    }
+?>
