c60738bad6cdb9ede48ebaa6622ec48e8393d620
cphillipp <clay@delorum.com>
2011-10-28 22:49:45 -0600

    added stuffcssoff

---
+++ cssoff/HTML/javascripts/index.js
@@ -0,0 +1,8 @@
+$(document).ready(function() {
+	$('.scrollPage').click(function() {
+	   var elementClicked = $(this).attr("href");
+	   var destination = $(elementClicked).offset().top;
+	   $("html:not(:animated),body:not(:animated)").animate({ scrollTop: destination-20}, 500 );
+	   return false;
+	});
+});
