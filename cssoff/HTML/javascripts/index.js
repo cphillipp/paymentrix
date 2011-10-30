976856e5c0ab1b234cf42743e60795b4649de0e3
unknown <phillipp@.(none)>
2011-10-30 03:13:41 -0600

    added some js and fixed some style issues at home

--- cssoff/HTML/javascripts/index.js
+++ cssoff/HTML/javascripts/index.js
@@ -1,8 +1,18 @@
 $(document).ready(function() {
+	//On click Event Nav
 	$('header nav p a').click(function() {
 	   var elementClicked = $(this).attr("href");
 	   var destination = $(elementClicked).offset().top;
 	   $("html:not(:animated),body:not(:animated)").animate({ scrollTop: destination-0}, 500 );
 	   return false;
 	});
+	
+	//On click Event Obstacle
+	$(".obstacles li").click(function() {
+		$(".obstacles li").removeClass("active"); //Remove any "active" class
+		$(this).addClass("active"); //Add "active" class to selected activity
+	});
+	
+	//Uses replaceholder library to fix non supporting browsers
+	$.fn.replaceholder();
 });
