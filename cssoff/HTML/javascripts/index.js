19c71bc6e8bb0765f91c1ac6db18c7702d432c26
cphillipp <clay@delorum.com>
2011-11-01 21:55:08 -0600

    updated cssoff fixes

--- cssoff/HTML/javascripts/index.js
+++ cssoff/HTML/javascripts/index.js
@@ -5,12 +5,12 @@ $(document).ready(function() {
 	   var destination = $(elementClicked).offset().top;
 	   $("html:not(:animated),body:not(:animated)").animate({ scrollTop: destination-0}, 500 );
 	   return false;
-	});	
+	});
 	//On click Event Obstacle
 	$(".obstacles li").click(function() {
 		$(".obstacles li").removeClass("active");
 		$(this).addClass("active");
-	});	
+	});
 	//Countdown of the clock
 	$(function() {
 		var seconds = 60;
@@ -23,6 +23,11 @@ $(document).ready(function() {
 			else{seconds = 60}
 			setTimeout(updateCountdown, 1000);
 		}
+	});
+	//On submit Event Form submission
+	$("#register").submit(function() {
+		alert('We require only your photo and email, and now its too late to turn back...your submission is accepted!');
+		return false;
 	});	
 	//Uses replaceholder library to fix non supporting browsers
 	$.fn.replaceholder();
