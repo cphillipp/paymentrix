e3ddb77a6eeaf92433a107a32787a8b4d4f04e00
cphillipp <clay@delorum.com>
2011-11-01 19:53:53 -0600

    updated cssoff clock

--- cssoff/HTML/javascripts/index.js
+++ cssoff/HTML/javascripts/index.js
@@ -5,27 +5,25 @@ $(document).ready(function() {
 	   var destination = $(elementClicked).offset().top;
 	   $("html:not(:animated),body:not(:animated)").animate({ scrollTop: destination-0}, 500 );
 	   return false;
-	});
-	
+	});	
 	//On click Event Obstacle
 	$(".obstacles li").click(function() {
-		$(".obstacles li").removeClass("active"); //Remove any "active" class
-		$(this).addClass("active"); //Add "active" class to selected activity
-	});
-	
+		$(".obstacles li").removeClass("active");
+		$(this).addClass("active");
+	});	
 	//Countdown of the clock
 	$(function() {
 		var seconds = 60;
 		setTimeout(updateCountdown, 1000);
 		function updateCountdown() {
 			seconds--;
-			if (seconds > 0) {
+			if (seconds >= 0) {
 				$(".clock p").text(seconds);
-				setTimeout(updateCountdown, 1000);
 			}
+			else{seconds = 60}
+			setTimeout(updateCountdown, 1000);
 		}
-	});
-	
+	});	
 	//Uses replaceholder library to fix non supporting browsers
 	$.fn.replaceholder();
 });
